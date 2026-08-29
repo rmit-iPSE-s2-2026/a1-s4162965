ed8bc7113598bb6056898ebae901fa1fe76b5248
Haribaskaran Premanand <s4162965@student.rmit.edu.au>
2026-08-29 19:08:09 +1000

    Initial Commit

---
+++ SplitMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplitMate
+//
+//  Created by Kirthika on 29/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplitMate/SplitMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitMateApp.swift
+//  SplitMate
+//
+//  Created by Kirthika on 29/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SplitMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
